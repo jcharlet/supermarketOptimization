311f1450169358e509094889500043dc2ddb11c8
Jeremie <charlet.jeremie@gmail.com>
2019-07-03 08:37:10 +0100

    add: first commit with .gitignore and hello world class & junit test

---
+++ src/main/java/ex1/Ex1.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        Ex1 ex1 = new Ex1();
+        System.out.println(ex1.returnHelloWorld());
+    }
+
+
+    public String returnHelloWorld() {
+        return "Hello World";
+    }
+}
