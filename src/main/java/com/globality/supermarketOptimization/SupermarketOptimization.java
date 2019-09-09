4167abc55f5ccb46f730457f92c79cc98636513e
Jeremie <charlet.jeremie@gmail.com>
2019-09-09 17:03:36 +0200

    add: filter product lines by nb of items per line + filter product ids by nb of occurrences per id

--- src/main/java/com/globality/supermarketOptimization/SupermarketOptimization.java
+++ src/main/java/com/globality/supermarketOptimization/SupermarketOptimization.java
@@ -2,14 +2,16 @@ package com.globality.supermarketOptimization;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SupermarketOptimization {
+    public static final int MIN_NUMBER_OF_IDS_PER_LINE = 3;
+    ConcurrentHashMap<String,Integer> mapOfProducts = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws IOException {
         String fileName = args[0];
@@ -19,15 +21,56 @@ public class SupermarketOptimization {
     }
 
     public String analyseFile(String fileName, Integer sigma) throws IOException{
-        try(Stream<String> lines = Files.lines(new File(fileName).toPath())){
-            return lines.map(line -> {
-                return process(line);
-            }).collect(Collectors.joining( System.lineSeparator()));
+        try(Stream<String> linesStream = Files.lines(new File(fileName).toPath())){
+            List<String> linesFilteredOnNumberOfElements = filterLinesByNumberOfIdsPerLine(linesStream, MIN_NUMBER_OF_IDS_PER_LINE);
+
+            populateProductsCountMap(linesFilteredOnNumberOfElements.stream());
+
+            List<String> linesWithIdsFilteredByTotalCount = filterIdsWhichOccurLessThanSigma(linesFilteredOnNumberOfElements.stream(), sigma);
+
+            List<String> linesPrepared = filterLinesByNumberOfIdsPerLine(linesWithIdsFilteredByTotalCount.stream(), MIN_NUMBER_OF_IDS_PER_LINE);
+
+            return linesPrepared.stream()
+                    .collect(Collectors.joining( System.lineSeparator()));
         }
     }
 
-    private String process(String line) {
-        return line;
+    private List<String> filterIdsWhichOccurLessThanSigma(Stream<String> linesStream, Integer sigma) {
+        return linesStream
+                .map(line -> {
+                    List<String> ids = Stream.of(line.split(" "))
+                            .filter(id -> id != null && mapOfProducts.get(id) != null && mapOfProducts.get(id) >= sigma)
+                            .collect(Collectors.toList());
+                    if(ids.size()==0){
+                        return null;
+                    }
+                    return ids.stream().collect(Collectors.joining(" "));
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> filterLinesByNumberOfIdsPerLine(Stream<String> linesStream, int minNumberOfIdsPerLine) {
+        return linesStream
+                .map(line -> {
+                    String[] products = line.split(" ");
+                    if(products.length< minNumberOfIdsPerLine){
+                        return null;
+                    }
+                    return line;
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    private void populateProductsCountMap(Stream<String> linesStream) {
+        linesStream
+            .forEach(line -> {
+                String[] products = line.split(" ");
+                Stream.of(products).forEach(id -> {
+                    mapOfProducts.compute(id, (k, v) -> v == null ? 1 : v + 1);
+                });
+            });
     }
 
 }
--- src/test/java/com/globality/supermarketOptimization/SupermarketOptimizationTest.java
+++ src/test/java/com/globality/supermarketOptimization/SupermarketOptimizationTest.java
@@ -14,9 +14,10 @@ class SupermarketOptimizationTest {
 
 
     public static final String TEST_FILE_PATH = "retail_10.dat";
+    public static final String COMPLETE_TEST_FILE_PATH = "retail_25k.dat";
 
     @Test
-    void testThatICanReturnHelloWorld() throws Exception {
+    void testThatICanFilterTheFile() throws Exception {
         //given class
         SupermarketOptimization supermarketOptimization = new SupermarketOptimization();
 
@@ -26,8 +27,23 @@ class SupermarketOptimizationTest {
 
         // then I get expected value
         File file = new File(getClass().getClassLoader().getResource(TEST_FILE_PATH).getFile());
-        assertEquals( new String (Files.readAllBytes(file.toPath())),output);
+        assertEquals( "38 39 48\n" +
+                "38 39 48",output);
     }
+
+//    @Test
+//    void testThatICanProcessTheCompleteFile() throws Exception {
+//        //given class
+//        SupermarketOptimization supermarketOptimization = new SupermarketOptimization();
+//
+//        //when I execute function
+//        String filePath = Paths.get(getClass().getClassLoader().getResource(COMPLETE_TEST_FILE_PATH).getFile()).toAbsolutePath().toString();
+//        String output = supermarketOptimization.analyseFile(filePath,3);
+//
+//        // then I get expected value
+//        File file = new File(getClass().getClassLoader().getResource(COMPLETE_TEST_FILE_PATH).getFile());
+//        assertEquals( new String (Files.readAllBytes(file.toPath())),output);
+//    }
 
 
 
